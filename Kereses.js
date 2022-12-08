f35ae4b881a0cf1156703642356924d60a7bd4cf
Porrobert <94959786+Porrobert@users.noreply.github.com>
2022-12-08 11:39:48 +0100

    HIBAK HIBAK HIBAK
    text string problema marvel szavazas mintajara probalkozas HIBAK

--- Kereses.js
+++ Kereses.js
@@ -46,31 +46,35 @@ export default class App extends Component {
   componentDidMount() {
     this.getMovies();
   }
-/*
-  szavazat=(szam)=>{
-    //alert(szam)
-    var adatok={
-      bevitel1:szam
-    }
-    alert(adatok.bevitel1)
-    const response = fetch('http://192.168.6.3:3000/szavazat',{
-      method: "POST",
-      body: JSON.stringify(adatok),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    });
-      const text =  response.text();
-      console.log(text)
-  }
 
-*/
+  
+osszeg=(szam)=>{
+  //alert(szam)
+  var adatok={
+    bevitel1:szam
+  }
+  alert(adatok.bevitel1)
+  const response = fetch(IP.ipcim+'kiadas',{
+    method: "POST",
+    body: JSON.stringify(adatok),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  });
+    const text =  response.text();
+    console.log(text)
+}
+
+
+
   render() {
+
+    
+
+
     const { data, isLoading } = this.state;
 
     return (
       <View style={{ flex: 1, padding: 24 , marginTop:40}}>
         {isLoading ? <ActivityIndicator/> : (
-
-          
           <FlatList
             data={data}
             keyExtractor={({ kiadas_id }, index) => kiadas_id}
@@ -92,13 +96,9 @@ export default class App extends Component {
         >
           <Text style={{color:'white',fontSize:30}}>Keresés</Text>
         </TouchableOpacity>
-
-              
-
-
               <Text style={{fontSize:30,color:'darkred',textAlign:'center',flex:1}}>
                 {item.fajta_nev}
-              </Text>
+                </Text>
 
 
 
@@ -125,15 +125,49 @@ export default class App extends Component {
         >
           <Text style={{color:'white',fontSize:30}}>Hozzáadás</Text>
         </TouchableOpacity>
-                  
+
+
+
+        <View style={{ flex: 1, padding: 24 , marginTop:40}}>
+        
+        <FlatList
+          
+          keyExtractor={({ fajta_id }, index) => fajta_id}
+          renderItem={({ item }) => (
+
+            <View style={{marginBottom:30}}>
+              <View>
+              <Text style={{fontSize:30,color:'darkred',textAlign:'center'}}>{item.fajta_nev}</Text>
+              </View>
+              <Image   source={{uri:IP.ipcim+item.fajta_kep}} style={{width:300,height:300,alignSelf:'center'}}   />
+              <TouchableOpacity style={styles.button} onPress={async ()=>this.szavazat(item.fajta_id)}>
+              <View>
+                <Text style={{color:'white',fontSize:30}}>Összeg hozzáadása</Text>
+              </View>
+              </TouchableOpacity>           
+            </View>
+          )}
+        />
+      )
+    </View>
+
               </View>
             )}
           />
         )}
       </View>
+
+
+
+
     );
   }
 };
+
+
+  
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
